91822f3b70846fc6f8c030d073bc3eb0ce66ce0c
Johanna Galli <galli.johanna.g2@gmail.com>
2020-02-05 15:24:26 +0100

    affichage fonctionnel du formulaire de 'connexion' et 's'enregistrer'

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once('Models/Users.php');
+
 class UsersController extends Controller
 {
-
    private $admin;
    private $pseudo;
    private $mdp;
@@ -47,24 +48,32 @@ class UsersController extends Controller
    }
 
 
-
-   private $register = "créer un compte";
-
-
    // Affichage du template
+
+
+
+
    public function index($slug = null)
+   //Dans mon index, si slug est null on affiche le formulaire de connexion
    {
+      //$slug est null
       $title = "Connexion";
 
-      if ($slug === "register"){
+
+      //si slug = register alors change le $title en "inscription".
+      if ($slug === "register") {
          $title = "Inscription";
       }
 
-      if(isset($slug) && $slug !== "register") {
+
+      //si slug est défini et différent de "register" (en gros si l'utilisateur met nimp dans l'url) alors : 
+      if (isset($slug) && $slug !== "register") {
+         //Affiche une erreur 303 dans la console :
          header("HTTP/1.0 303 Redirection");
+         //Fait une redirection vers la page d'accueil :
          header("Location: $this->baseUrl");
-         
-      }      
+      }
+
       $pageTwig = 'Users/index.html.twig';
       $template = $this->twig->load($pageTwig);
       echo $template->render(['slug' => $slug, "title" => $title]);
---
+++ Models/Users.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+//connexion bdd
+class User extends Model
+{ 
+   public function __construct()
+   {
+      $this->pdo = parent::getPdo();
+   }
+
+   public function getpseudo()
+   {
+      $req = $this->pdo->prepare('SELECT FROM `users` WHERE `pseudo`');
+      $req->execute();
+      return $req->fetchAll();
+   }
+}
+
+  
+
