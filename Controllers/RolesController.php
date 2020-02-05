4c7629a34cb82b2af7da159d16bcac156846630b
Thomas Loegel <t.loegel@gmail.com>
2020-02-05 13:45:19 +0100

    Del -> Setter / Getter

--- Controllers/RolesController.php
+++ Controllers/RolesController.php
@@ -1,5 +1,5 @@
 <?php
-class RolesController
+class RolesController extends Controller
 {
     private $id_role;
     private $id_artist;
@@ -11,27 +11,7 @@ class RolesController
 
     public function __construct()
     {
-        $this->actor    = $actor;
-        $this->director = $director;
+
     }
 
-    // Setter
-    public function setActor($actor)
-    {
-        $this->actor = $actor;
-    }
-    public function setDirector($director)
-    {
-        $this->director = $director;
-    }
-
-    // Getter
-    public function getActor($actor)
-    {
-        return $this->actor;
-    }
-    public function getDirector($director)
-    {
-        return $this->director;
-    }
 }
