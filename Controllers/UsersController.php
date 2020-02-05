b9f39a6cf01487dfd26962ef76bbf1a8641ff157
Johanna Galli <galli.johanna.g2@gmail.com>
2020-02-05 12:01:50 +0100

    mise en place des formuaires de login et register avec twig

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -1,27 +1,72 @@
 <?php
 
-class User
+class UsersController extends Controller
 {
 
-    private $admin;
-    private $pseudo;
-    private $mdp;
-    private $mail;
+   private $admin;
+   private $pseudo;
+   private $mdp;
+   private $mail;
+
+   public function __construct()
+   {
+      //$this->twig = parent::getTwig();
+      parent::__construct();
+   }
+
+   // Setter
+   public function setAdmin($admin)
+   {
+      $this->admin = $admin;
+   }
+   public function setPseudo($pseudo)
+   {
+      $this->pseudo = $pseudo;
+   }
+   public function setMdp($mdp)
+   {
+      $this->mdp = $mdp;
+   }
+
+   // Getter
+   public function getAdmin()
+   {
+      echo $this->admin;
+   }
+   public function getPseudo()
+   {
+      echo $this->pseudo;
+   }
+   public function getMdp()
+   {
+      echo $this->mdp;
+   }
+   public function getMail()
+   {
+      echo $this->mail;
+   }
 
 
-    public function set($admin, $pseudo, $mdp)
-    {
-        $this->$admin = $admin;
-        $this->$pseudo = $pseudo;
-        $this->$mdp = $mdp;
-    }
 
-    public function get()
-    {
-        echo $this->admin;
-        echo $this->pseudo;
-        echo $this->mdp;
-        echo $this->mail;
-    }
+   private $register = "créer un compte";
 
+
+   // Affichage du template
+   public function index($slug = null)
+   {
+      $title = "Connexion";
+
+      if ($slug === "register"){
+         $title = "Inscription";
+      }
+
+      if(isset($slug) && $slug !== "register") {
+         header("HTTP/1.0 303 Redirection");
+         header("Location: $this->baseUrl");
+         
+      }      
+      $pageTwig = 'Users/index.html.twig';
+      $template = $this->twig->load($pageTwig);
+      echo $template->render(['slug' => $slug, "title" => $title]);
+   }
 }
