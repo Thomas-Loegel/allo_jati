1c20b2d61ddcb25a93608c41c8db66b52cd95f7d
Thomas Loegel <t.loegel@gmail.com>
2020-02-06 21:15:01 +0100

    Update de vue fim et vue artist

--- Controllers/ArtistsController.php
+++ Controllers/ArtistsController.php
@@ -2,36 +2,37 @@
 
 class ArtistsController extends Controller
 {
-    private $id_artist;
-    private $role;
-    private $picture;
-    private $first_name;
-    private $last_name;
-    private $birth_day;
-    private $bio;
+   private $id_artist;
+   private $role;
+   private $picture;
+   private $first_name;
+   private $last_name;
+   private $birth_day;
+   private $bio;
 
-    public function __construct()
-    {
-        $this->twig = parent::getTwig();
-        parent::__construct();
-        $this->model = new Artists();
-    }
+   public function __construct()
+   {
+      $this->twig = parent::getTwig();
+      parent::__construct();
+      $this->model = new Artists();
+   }
 
-    // Affichage du template + Tous les Artistes
-    public function index()
-    {
-        $result   = $this->model->getAllArtists();
-        $pageTwig = 'Artists/index.html.twig';
-        $template = $this->twig->load($pageTwig);
-        echo $template->render(['result' => $result]);
-    }
 
-    // Affichage du template + Tous les Artistes du Film
-    public function showByMovie()
-    {
-        $result   = $this->model->getByFilm();
-        $pageTwig = 'Artists/showByMovie.html.twig';
-        $template = $this->twig->load($pageTwig);
-        echo $template->render(["result" => $result]);
-    }
+   // Affiche la fiche Artiste
+   public function showArtist($id_artist)
+   {
+   $artist   = $this->model->getArtist($id_artist);
+   $pageTwig = 'Artists/showArtist.html.twig';
+   $template = $this->twig->load($pageTwig);
+   echo $template->render(['artist' => $artist]);
+   }
+
+   // Affiche tout les Artistes du Film
+   public function showByMovie()
+   {
+      $artists = $this->model->getByFilm();
+      $pageTwig = 'Artists/showByMovie.html.twig';
+      $template = $this->twig->load($pageTwig);
+      echo $template->render(["artists" => $artists]);
+   }
 }
--- Models/Movies.php
+++ Models/Movies.php
@@ -2,23 +2,27 @@
 
 class Movies extends Model
 {
-    public function __construct()
-    {
-        $this->pdo = parent::getPdo();
-    }
+   public function __construct()
+   {
+      $this->pdo = parent::getPdo();
+   }
 
-    // Recupère tout les Films
-    public function getAllMovies()
-    {
-        $req = $this->pdo->prepare('SELECT * FROM movies');
-        $req->execute();
-        return $req->fetchAll();
-    }
+   // Recupère tout les Films
+   public function getAllMovies()
+   {
+      $req = $this->pdo->prepare('SELECT * FROM movies');
+      $req->execute();
+      return $req->fetchAll();
+   }
 
-    // Recupère un Film avec ID
-    public function getMovie($id_movie) {
-        $req = $this->pdo->prepare('SELECT * FROM movies WHERE movies.id_movie = ? AND movies.id_movie = movies.id_movie ');
-        $req->execute([$id_movie]);
-        return $req->fetch();
-    }
+   // Recupère un Film avec ID
+   public function getMovie($id_movie) {
+      $req = $this->pdo->prepare(
+        'SELECT *
+         FROM movies
+         WHERE movies.id_movie = ?
+         AND movies.id_movie = movies.id_movie ');
+      $req->execute([$id_movie]);
+      return $req->fetch();
+   }
 }
