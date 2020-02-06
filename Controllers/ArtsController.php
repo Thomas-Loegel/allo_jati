c5805ea2a7c7795159dc6adc0a064159802d9a7d
Thomas Loegel <t.loegel@gmail.com>
2020-02-06 11:51:57 +0100

    Changement des nom de classe et fonction qui colle avec la Bdd + Indentation & commentaires

--- Controllers/ArtsController.php
+++ Controllers/ArtsController.php
@@ -1,53 +1,15 @@
 <?php
 class ArtsController extends Controller
 {
-   private $id_works;
-   private $type;
+   private $id_movie;
    private $title;
    private $year;
    private $style;
 
    public function __construct()
    {
-      $this->twig = parent::getTwig();
-      parent::__construct();
-      $this->model = new Works();
-   }
-   /** Setter*/ 
-
-   public function setType($value)
-   {
-      $this->type = $value;
-   }
-   public function setTitle($value)
-   {
-      $this->title = $value;
-   }
-   public function setYear($value)
-   {
-      $this->year = $value;
-   }
-   public function setStyle($value)
-   {
-      $this->style = $value;
-   }
-
-   /** Getter*/ 
-
-   public function getType()
-   {
-      return $this->type;
-   }
-   public function getTitle()
-   {
-      return $this->title;
-   }
-   public function getYear()
-   {
-      return $this->year;
-   }
-   public function getStyle()
-   {
-      return $this->style;
+       $this->twig = parent::getTwig();
+       parent::__construct();
+       $this->model = new Movies();
    }
 }
--- index.php
+++ index.php
@@ -5,15 +5,13 @@ require_once 'vendor/autoload.php';
 $router = new Router($_GET['url']);
 
 // listes de nos routes
-$router->get('/', 'Home.index');
 $router->get('/Artists/show', 'Artists.setByArtists');
 $router->get('/Artists', 'Artists.index');
-$router->get('/Films', 'Works.index');
-$router->get('/Films/Show/:id_works', 'Works.showmovie');
-
-
+$router->get('/Films/Film/:id_movie', 'Movies.showOneMovie');
+$router->get('/Films', 'Movies.showAllMovies');
 $router->get('/Users/:slug', 'Users.index');
 $router->get('/Users', 'Users.index');
+$router->get('/', 'Home.index');
 
 
 $router->run();
