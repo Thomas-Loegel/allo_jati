ed7a6e3f9ca317a4172a21f192d871b582d8eb99
Thomas Loegel <t.loegel@gmail.com>
2020-02-06 09:49:10 +0100

    Ajout lien header

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $router = new Router($_GET['url']);
 
 // listes de nos routes
 $router->get('/', 'Home.index');
+$router->get('/Artists/show', 'Artists.index');
 $router->get('/Artists', 'Artists.index');
 $router->get('/Users/:slug', 'Users.index');
 $router->get('/Users', 'Users.index');
