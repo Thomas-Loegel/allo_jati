df0722235bb094d0bcdd73c92a1af84b1cd9e844
jad67tony <anthony.montmirail@gmail.com>
2020-02-07 20:00:15 +0100

    Avancé sur CommentControler et pull des nouveaux Ajout

--- Models/Comments.php
+++ Models/Comments.php
@@ -11,15 +11,16 @@ class Comments extends Model
    {
       $req = $this->pdo->prepare('SELECT * FROM comments');
       $resultat = $req->execute();
-      //var_dump($resultat);
+
       return $req->fetchAll();
    }
+
    //recherche un commentaire par id_user
    public function getOneComment($id)
    {
       $req = $this->pdo->prepare('SELECT * FROM comments WHERE id_user= ?');
       $resultat = $req->execute([$id]);
-      //var_dump($resultat);
+
       return $req->fetchAll();
    }
    //suppression commentaire par id_comment
@@ -27,11 +28,26 @@ class Comments extends Model
    {
       $req = $this->pdo->prepare('DELETE FROM comments WHERE id_comment = ?');
       $result = $req->execute([$id]);
-      //var_dump($result);
+
       return $result;
    }
+
+   //Ajouter un commentaire
+   public function addComment(){
+      $req = $this->pdo->prepare('INSERT INTO comments (id_user, title, content, note, date
+      VALUE (:id_user, :title, :content, :note, :date');
+      $result = $req->execute();
+
+      return $result;
+   }
+
+
+
+
+
+
    //recherche la liste des commentaire par id_movie
-   public function linkCommentWorks($id)
+   public function linkCommentByMovie($id)
    {
       $req = $this->pdo->prepare(
          'SELECT comments.* 
@@ -40,7 +56,7 @@ class Comments extends Model
          AND movies.id_movie = movie_comments.id_movie 
          AND comments.id_comment = movie_comments.id_comment');
       $req->execute([$id]);
-      //var_dump($req->fetchAll());
+
       return $req->fetchAll();
    }
 }
--- index.php
+++ index.php
@@ -5,14 +5,18 @@ $router = new Router($_GET['url']);
 
 // listes des routes
 $router->get('/Films/Film_:id_movie', 'Movies.showOneMovie');
+
+$router->post('/Comments/addComment/:id_movie', 'Comments.addComment');
+
 $router->get('/Films/Artiste_:id_artist', 'Artists.showArtist');
-$router->get('Comments/Delete_:id_movie', 'Comments.delAllComByMovie');
+$router->get('/Comments/Delete_:id_movie', 'Comments.delAllComByMovie');
 $router->get('/Connexion/:slug', 'Users.index');
-$router->get('Comments/GetAll', 'Comments.getAllCom');
+$router->get('/Comments/GetAll', 'Comments.getAllCom');
 $router->get('/Films', 'Movies.showAllMovies');
 $router->get('/Connexion', 'Users.index');
 $router->get('/Artists', 'Artists.index');
 $router->get('/Comments', 'Comments.index');
+
 
 $router->get('/', 'Home.index');
 $router->run();
