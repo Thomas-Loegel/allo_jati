fd4ce651ed5de5f27efaa8cb6789dc2908027a74
Thomas Loegel <t.loegel@gmail.com>
2020-02-18 16:36:45 +0100

    formualaire admin Film ok avec feedback erreur

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -79,38 +79,48 @@ class AdminController extends Controller
    public function addMovie()
    {
       $slug = 'Ajout_Film';
-
-      $success = null;
       $error   = null;
-      $picture = null;
-      $title   = null;
-      $year    = null;
-      $style   = null;
-      $resume  = null;
-      $time    = null;
+      $success = null;
+      $title = $year = $time = $picture = $style = $resume= null;
+      $pregYear = '/^[0-9]{4}$/m';
+      $pregTime = '/^([01]?[0-9]|2[0-3])\:+[0-5][0-9]$/';
+      $pregUrl = '%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu';
 
-      if (!empty($_POST)){
+
+      if (isset($_POST['title']) && !empty($_POST['title'])) {
+         $title = $_POST['title'];
+      }else {
+         $error = "Titre non renseigné !";
+      }
+
+      if (isset($_POST['year']) && !empty($_POST['year']) && preg_match($pregYear,$_POST['year'])) {
+         $year = $_POST['year'];
+      }else {
+         $error = "Format date non valide (YYYY) !";
+      }
+
+      if (isset($_POST['time']) && !empty($_POST['time']) && preg_match($pregTime,$_POST['time'])) {
+         $time = $_POST['time'];
+      }else {
+         $error = "Format durée non valide (H:MM)!";
+      }
+
+      if (isset($_POST['picture']) && !empty($_POST['picture']) && preg_match($pregUrl,$_POST['picture'])){
          $picture = $_POST['picture'];
-         $title   = $_POST['title'];
-         $year    = $_POST['year'];
-         $style   = $_POST['style'];
-         $resume  = $_POST['resume'];
-         $time    = $_POST['time'];
+      }else {
+         $error = "Format url non valide !";
+      }
 
-         if (!empty($picture) && !empty($title) && !empty($year) && !empty($style) && !empty($resume) && !empty($time)){
-               $success = null;
-               $error   = null;
-               $picture = null;
-               $title   = null;
-               $year    = null;
-               $style   = null;
-               $resume  = null;
-               $time    = null;
+      if (isset($_POST['style']) && !empty($_POST['style'])){
+         $style = $_POST['style'];
+      }else {
+         $error = "Style non renseigné !";
+      }
 
-               $success = "Votre film à bien été rajouté à la base de données !";
-         }else {
-            $error = "Veuillez remplir tous les champs !";
-         }
+      if (isset($_POST['resume']) && !empty($_POST['resume'])){
+         $resume = $_POST['resume'];
+      }else {
+         $error = "Résumé non renseigné !";
       }
 
       $addMovie = $this->model->addMovie($picture,$title,$year,$style,$resume,$time);
@@ -118,12 +128,12 @@ class AdminController extends Controller
       $template = $this->twig->load($pageTwig);
       echo $template->render([
          'slug'    => $slug,
-         'picture' => $picture,
          'title'   => $title,
          'year'    => $year,
+         'time'    => $time,
+         'picture' => $picture,
          'style'   => $style,
          'resume'  => $resume,
-         'time'    => $time,
          'error'   => $error,
          'success' => $success
       ]);
