3bddc0f5fa4d5524e1422edab0cb3622021e1a13
Johanna Galli <galli.johanna.g2@gmail.com>
2020-02-19 11:34:38 +0100

    enlevé les vardump

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -12,8 +12,7 @@ class Controller
       $this->twig = self::getTwig();
       $this->baseUrl = self::getBaseUrl();
       session_start();
-      var_dump($_SESSION['utilisateur']);
-
+      //var_dump($_SESSION['utilisateur']);
    }
 
    protected static function getTwig()
