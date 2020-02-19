1425d0609414de75d556e63acda445430d1a7f00
ilias-imoula <iliasimoula97@gmail.com>
2020-02-19 10:14:24 +0100

    termin suggestion

--- Controllers/SuggestionController.php
+++ Controllers/SuggestionController.php
@@ -27,7 +27,46 @@ class SuggestionController extends Controller
       $pseudo = $_POST['pseudo'];
       $email = $_POST['email'];
       $textArea = $_POST['text'];
-      $file = $_POST['file'];
+
+      //function pour fichier zip
+      if ($_FILES && $_FILES['zip_file']) {
+
+         if (!empty($_FILES['zip_file']['name'][0])) {
+     
+            $zip = new ZipArchive();
+            $zip_name = "assets/FileZip/FileZip" .rand(0 , 999). ".zip";
+            
+            // Create a zip target
+            if ($zip->open($zip_name, ZipArchive::CREATE) !== TRUE) {
+               $error .= "Sorry ZIP creation is not working currently.<br/>";
+            }
+
+            $imageCount = count($_FILES['zip_file']['name']);
+            for ($i = 0; $i < $imageCount; $i++) {
+
+               if ($_FILES['zip_file']['tmp_name'][$i] == '') {
+                     continue;
+               }
+               $newname = date('YmdHis', time()) . mt_rand() . '.jpg';
+
+               // Moving files to zip.
+               $zip->addFromString($_FILES['zip_file']['name'][$i], file_get_contents($_FILES['zip_file']['tmp_name'][$i]));
+            }
+
+            $zip->close();
+
+            // Create HTML Link option to download zip
+            $success = basename($zip_name);
+            
+         } else {
+            $error = '<strong>Error!! </strong> Please select a file.';}                  
+      }
+
+      if (isset($zip_name)){
+         $download = " <a href = http://localhost/allo_jati/$zip_name ><h1>Download</h1> </a>";
+      }else{
+         $download = "";
+      }
 
       if ($_SERVER['SERVER_NAME'] == 'localhost'){
          
@@ -42,21 +81,18 @@ class SuggestionController extends Controller
          $message = (new Swift_Message('Suggestion site Allo_Jati'))
             ->setFrom([$email => $pseudo])
             ->setTo(['receiver@suggestion.org', 'allo_jati@suggestion.org' => 'A name'])
-            ->setBody($textArea.$file);
-
+            ->setBody($textArea.$download, 'text/html');
+        
          $result = $mailer->send($message);
-         
-         if ($result){
-            $ok =  'merci!!!!!';
-                    
-         }else{
-            $ok =  'errrorrrrr';
 
-      }
+         if ($result){
+            $alertsEmail = 'yes';
+         } else {
+            $alertsEmail = 'no';
+         }
+                 
       $pageTwig = 'Suggestion/suggestion.html.twig';
       $template = $this->twig->load($pageTwig);
-      echo $template->render(["ok" =>$ok ]);
+      echo $template->render(["alertsEmail" =>$alertsEmail]);
    }          
 }
-
-
